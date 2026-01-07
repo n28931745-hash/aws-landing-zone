0c11fb9892b554be2a80cdd7b04ec23d8d1d5960
n28931745-hash <n28931745@gmail.com>
2026-01-07 21:22:57 +0530

    Update main.tf

--- terraform/00-bootstrap-terraform/main.tf
+++ terraform/00-bootstrap-terraform/main.tf
@@ -16,8 +16,7 @@ resource "aws_s3_bucket" "tf_state"{
     bucket = var.tf_state_bucket_name
 }
 
-resource "aws_s3_bucket_versioning" "tf_state_versioning"
-{
+resource "aws_s3_bucket_versioning" "tf_state_versioning"{
     bucket = aws_s3_bucket.tf_state.id
     versioning_configuration{
         status = "Enabled"
